e3e1fb94db90d6d450650f8730458391c8ad1f37
Damian Pulido <72259251+damian-po@users.noreply.github.com>
2023-04-28 17:00:16 +0000

    Created java project, moved soruce files to .src

---
+++ M5UF2_ProjecteJavaG3/src/Tests.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class Tests {
+    private final Main main = new Main();
+
+    @Rule
+    public testFile resurce(List<string> yea)
+    {
+
+    }
+    
+    
+}
