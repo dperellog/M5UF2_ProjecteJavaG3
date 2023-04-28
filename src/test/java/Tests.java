9a324128398d5943c26ce097700167c7fd7da039
Damian Pulido <72259251+damian-po@users.noreply.github.com>
2023-04-28 17:21:43 +0000

    Refactor to repo file structure

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,6 +1,9 @@
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
+import main.Main;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -9,13 +12,5 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class Tests {
-    private final Main main = new Main();
-
-    @Rule
-    public testFile resurce(List<string> yea)
-    {
-
-    }
-    
     
 }
