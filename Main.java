fe443626ff1410609a12062fc22f4f41cadbdcd4
dperellog <dperellog@gmail.com>
2023-05-19 18:26:54 +0200

    commit per sincronitzar

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    
+
+    private Pila historial;
+    private Pila eliminades;
+
+    private ArrayList<String> navegacio = new ArrayList<>();
+
+    public void input() {
+        this.navegacio.addAll(Arrays.asList("a", "b", "<", "c", "d","<",">"));
+    }
+
+    public static void main(String[] args) {
+
+        //INPUT: Definir la navegació de prova:
+        input()
+        
+    }
+
 }
